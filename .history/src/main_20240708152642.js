f1723fc97548808228f781b1a857b9857615f361
ddt2h <halifire@mail.ru>
2024-07-08 16:34:17 +0300

    working

--- .history/src/main_20240708152642.js
+++ .history/src/main_20240708152642.js
@@ -14,7 +14,8 @@ const pendingRequests = [];
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '250mb'}));
+app.use(express.json());
+app.use(express.bodyParser({limit: '50mb'}));
 app.use(express.static(path.join(__dirname, '../front')));
 
 getPage(app, __dirname);
